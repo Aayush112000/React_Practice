18758b71ab8ad942b9267ce2ffbea3eec4f69044
hardikt093 <107110863+hardikt093@users.noreply.github.com>
2024-04-03 14:16:19 +0530

    Upto build

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,18 +1,67 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar.tsx';
-// import TextFrom from './components/TextForm.tsx';
-import About from './components/About.tsx';
+import TextFrom from './components/TextForm.tsx';
+// import About from './components/About.tsx';
+import React from 'react';
+import Alert from './components/Alert.tsx';
+
+// import {
+//   BrowserRouter as Router,
+//   Switch ,
+//   // Routes,
+//   Route,
+//   // Link,
+// } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light')  //Whether dark mode is enable or not
+
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type)=>{
+    setAlert({
+        msg: message,
+        type: type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    }, 1500)
+}
+
+
+  const toggleMode = () =>{
+    if(mode==="light"){
+      setMode("dark")
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success")
+      document.title = "TextUtils - Dark Mode"
+    }
+    else{
+      setMode("light")
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success")
+      document.title = "TextUtils - Light Mode"
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils" aboutText="about textUtils"/>
-    {/* <Navbar/> */}
-    <div className="container my-3">
-      {/* <TextFrom heading="Enter the text to analyze below"/> */}
-      <About/>
-    </div>
+    {/* <Router> */}
+      <Navbar title="TextUtils" aboutText="about textUtils" mode={mode} toggleMode={toggleMode}/>
+      {/* <Navbar/> */}
+      <Alert alert={alert}/>
+      {/* <div className="container my-3"> */}
+        {/* <Switch> */}
+          {/* <Route exact path="/about"> */}
+            {/* <About /> */}
+          {/* </Route> */}
+          {/* <Route exact path="/"> */}
+            <TextFrom heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}/>
+          {/* </Route> */}
+        {/* </Switch > */}
+      {/* </div> */}
+    {/* </Router> */}
     </>
   );
 }
--- textutils/src/components/TextForm.tsx
+++ textutils/src/components/TextForm.tsx
@@ -3,56 +3,84 @@ import React, {useState} from 'react'
 export default function TextFrom(props) {
     const [text,setText] = useState('Enter text here')
     const handleUpClick = ()=>{
-        // console.log("Uppercase was clicked")
+        // console.log('Uppercase was clicked')
         let newText = text.toUpperCase()
         setText(newText)
+        props.showAlert("Converted to UpperCase!","sucess");
     }
     const handleOnChange = (event)=>{
-        // console.log("On Change Clicked")
+        // console.log('On Change Clicked')
         setText(event.target.value)
     }
     const handleLoClick = ()=>{
         let newText = text.toLowerCase()
         setText(newText)
+        props.showAlert("Converted to LowerCase!","sucess");
     }
     const handleClearClick = ()=>{
-        let newText = ""
+        let newText = ''
         setText(newText)
+        props.showAlert("Clear text!","sucess");
     }
     const handleFirstUpperClick = ()=>{
-        const words = text.split(" ")
-        if(words.length > 0){
-            for (let i = 0; i < words.length; i++) {
-                words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+        let newText="";
+        for(let word of text.split(" ")){
+            if(word){
+                newText = newText+ " " + word[0].toUpperCase() + word.slice(1).toLowerCase()
             }
-            let newText = words.join(" ");
-            setText(newText)
         }
-        else{
-            let newText = ""
-            setText(newText)
+        setText(newText)
+        props.showAlert("Converted first to UpperCase!","sucess");
+    }
+
+    const handleCopy = ()=>{
+        var text = document.getElementById('myBox');
+        // text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Text Copied!","sucess");
+
+    }
+
+    function wordCount(text){
+        let counter = 0
+        for(let word of text.split(" ")){
+            if(word){
+                counter = counter+1;
+            }
         }
+        return counter
+    }
+
+    function charCount(text){
+        let counter = 0
+        for(let c in text){
+            if(text[c]!==" "){
+                counter = counter+1;
+            }
+        }
+        return counter
     }
 
 
     return (
         <>
-        <div className='container'>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <div className='container' style={{color: props.mode==='dark'?'white':'black'}}>
+            <h1>{props.heading}</h1>    
+            <div className='mb-3'>
+            <textarea className='form-control' value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'gray':'white',color: props.mode==='dark'?'white':'black'}} id='myBox' rows='8'></textarea>
             </div>
-            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-            <button className="btn btn-primary mx-2" onClick={handleFirstUpperClick}>Capitalize Text</button>
+            <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to Lowercase</button>
+            <button className='btn btn-primary mx-2' onClick={handleClearClick}>Clear Text</button>
+            <button className='btn btn-primary mx-2' onClick={handleFirstUpperClick}>Capitalize Text</button>
+            <button className='btn btn-primary mx-2' onClick={handleCopy}>Copy Text</button>
         </div>
-        <div className="container my-3">
+        <div className='container my-3' style={{color: props.mode==='dark'?'white':'black'}}>
             <h2>Your Text Summary</h2>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <p>{wordCount(text)} words and {charCount(text)} characters</p>
+            <p>{0.008 * text.split(' ').length} Minutes read</p>
         </div>
-        <div className="container my-3">
+        <div className='container my-3' style={{color: props.mode==='dark'?'white':'black'}}>
             <h2>Preview</h2>
             <p>{text}</p>
         </div>
